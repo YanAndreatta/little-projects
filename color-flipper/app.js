9e8ab326f68866dd61cf58890e63666c889eafc2
Yan Andreatta <yan.duca2@gmail.com>
2024-02-29 19:02:02 -0300

    start project

---
+++ color-flipper/app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)","#f14025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+    // console.log(document.body);
+})
