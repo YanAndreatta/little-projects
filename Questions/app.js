110a41744e5935c3251ff7a303485e95b510f0d2
YanAndreatta <yan.duca2@gmail.com>
2024-03-05 20:28:43 -0300

    Finished Question project

--- Questions/app.js
+++ Questions/app.js
@@ -1,14 +1,30 @@
-const btnQuestion = document.querySelectorAll(".question-btn");
-const article = document.querySelectorAll(".question");
+// const btnQuestion = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 
-btnQuestion[0].addEventListener("click", () => {
-  article[0].classList.toggle("show-text");
+
+// btnQuestion[0].addEventListener("click", () => {
+//   questions[0].classList.toggle("show-text");
+// });
+
+// btnQuestion[1].addEventListener("click", () => {
+//   questions[1].classList.toggle("show-text");
+// });
+
+// btnQuestion[2].addEventListener("click", () => {
+//   questions[2].classList.toggle("show-text");
+// });
+
+questions.forEach(question => {
+  console.log(question);
+  const btn = question.querySelector(".question-btn");
+  console.log(btn);
+  btn.addEventListener("click", () => {
+    questions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
 });
 
-btnQuestion[1].addEventListener("click", () => {
-  article[1].classList.toggle("show-text");
-});
-
-btnQuestion[2].addEventListener("click", () => {
-  article[2].classList.toggle("show-text");
-});
